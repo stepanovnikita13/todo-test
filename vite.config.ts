14a5c4362ccb1af46166f7ee1d3787faf0c8f551
stepanovnikita13 <stepanov.nikita13@gmail.com>
2025-03-31 21:00:22 +0300

    Add to vite.config.js field 'base'

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-test',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
